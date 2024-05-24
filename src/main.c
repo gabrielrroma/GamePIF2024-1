23bd5762244daae76abdad09e716acbd1d107574
Gabriel Roma <100969556+gabrielrroma@users.noreply.github.com>
2024-05-24 17:02:58 -0300

    Initial game definitions

--- src/main.c
+++ src/main.c
@@ -1,88 +1,33 @@
-/**
- * main.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
-
-#include <string.h>
-
 #include "screen.h"
 #include "keyboard.h"
-#include "timer.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-int x = 34, y = 12;
-int incX = 1, incY = 1;
+#define PLAYER_UP 'w'
+#define PLAYER_DOWN 's'
+#define EXIT_KEY 'q'
+#define START_KEY 'p'
 
-void printHello(int nextX, int nextY)
-{
-    screenSetColor(CYAN, DARKGRAY);
-    screenGotoxy(x, y);
-    printf("           ");
-    x = nextX;
-    y = nextY;
-    screenGotoxy(x, y);
-    printf("Hello World");
-}
+#define GAME_WIDTH 60
+#define GAME_HEIGHT 20
 
-void printKey(int ch)
-{
-    screenSetColor(YELLOW, DARKGRAY);
-    screenGotoxy(35, 22);
-    printf("Key code :");
+#define BARRIER_SIZE 2
 
-    screenGotoxy(34, 23);
-    printf("            ");
-    
-    if (ch == 27) screenGotoxy(36, 23);
-    else screenGotoxy(39, 23);
+int maxPoints = 10;
 
-    printf("%d ", ch);
-    while (keyhit())
-    {
-        printf("%d ", readch());
-    }
-}
+typedef struct {
+    int x;
+    int y;
+    int dx;
+    int dy;
+} Ball;
 
-int main() 
-{
-    static int ch = 0;
+typedef struct {
+    int y;
+} Paddle;
 
-    screenInit(1);
-    keyboardInit();
-    timerInit(50);
-
-    printHello(x, y);
-    screenUpdate();
-
-    while (ch != 10) //enter
-    {
-        // Handle user input
-        if (keyhit()) 
-        {
-            ch = readch();
-            printKey(ch);
-            screenUpdate();
-        }
-
-        // Update game state (move elements, verify collision, etc)
-        if (timerTimeOver() == 1)
-        {
-            int newX = x + incX;
-            if (newX >= (MAXX -strlen("Hello World") -1) || newX <= MINX+1) incX = -incX;
-            int newY = y + incY;
-            if (newY >= MAXY-1 || newY <= MINY+1) incY = -incY;
-
-            printKey(ch);
-            printHello(newX, newY);
-
-            screenUpdate();
-        }
-    }
-
-    keyboardDestroy();
-    screenDestroy();
-    timerDestroy();
-
-    return 0;
-}
+typedef struct Barrier {
+    int x;
+    int y;
+    struct Barrier* next;
+} Barrier;
