b2dfe3f6da1e67f6c10884641b4e7d6089702aed
gabrielcorreiapontes <80489162+gabrielcorreiapontes@users.noreply.github.com>
2024-05-27 17:52:33 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -98,3 +98,118 @@ void addBarrierAfterPoint(Ball* ball, Barrier** barriers) {
     int barrierX = ball->x + (ball->dx > 0 ? 2 : -2);
     addBarrier(barriers, barrierX, ball->y);
 }
+void updateBall(Ball* ball, Paddle* player, Paddle* cpu, int* scorePlayer, int* scoreCPU, Barrier** barriers) {
+    ball->x += ball->dx;
+    ball->y += ball->dy;
+
+    if (ball->y <= 1 || ball->y >= GAME_HEIGHT - 2) {
+        ball->dy = -ball->dy;
+    }
+
+    if ((ball->x == 3 && ball->dx < 0 && ball->y >= player->y && ball->y < player->y + 4) ||
+        (ball->x == GAME_WIDTH - 4 && ball->dx > 0 && ball->y >= cpu->y && ball->y < cpu->y + 4)) {
+        if (ball->y >= player->y && ball->y < player->y + 4) {
+            ball->dx = -ball->dx;
+        } else if (ball->y >= cpu->y && ball->y < cpu->y + 4) {
+            ball->dx = -ball->dx;
+        }
+    }
+
+    checkBarrierCollision(ball, barriers);
+
+    if (ball->x <= 1) {
+        (*scoreCPU)++;
+        if (barrierMode) {
+            addBarrierAfterPoint(ball, barriers);
+        }
+        initGame(ball, player, cpu, barriers);
+    } else if (ball->x >= GAME_WIDTH - 1) {
+        (*scorePlayer)++;
+        if (barrierMode) {
+            addBarrierAfterPoint(ball, barriers);
+        }
+        initGame(ball, player, cpu, barriers);
+    }
+}
+
+void updatePlayer(Paddle* player) {
+    char key;
+
+    if (keyhit()) {
+        key = readch();
+
+        if (key == PLAYER_UP && player->y > 1) {
+            player->y--;
+        } else if (key == PLAYER_DOWN && player->y + 4 < GAME_HEIGHT) {
+            player->y++;
+        }
+
+        if (key == EXIT_KEY) {
+            screenDestroy();
+            keyboardDestroy();
+            exit(0);
+        }
+    }
+}
+
+void updateCPU(Paddle* cpu, Ball* ball) {
+    if (cpu->y + 2 < ball->y && cpu->y + 4 < GAME_HEIGHT) {
+        cpu->y += CPU_SPEED;
+    } else if (cpu->y > ball->y && cpu->y > 1) {
+        cpu->y -= CPU_SPEED;
+    }
+
+    if (rand() % 5 == 0) {
+        cpu->y += (rand() % 2 == 0) ? 1 : -1;
+    }
+}
+
+void drawGame(Ball* ball, Paddle* player, Paddle* cpu, int scorePlayer, int scoreCPU, Barrier** barriers) {
+    screenDrawBorders();
+
+    screenGotoxy(ball->x, ball->y);
+    printf("o");
+
+    for (int i = 0; i < 4; i++) {
+        screenGotoxy(3, player->y + i);
+        printf("|");
+
+        screenGotoxy(GAME_WIDTH - 4, cpu->y + i);
+        printf("|");
+    }
+
+    drawBarriers(*barriers);
+
+    screenGotoxy(GAME_WIDTH / 2 - 2, 1);
+    printf("Player: %d", scorePlayer);
+
+    screenGotoxy(GAME_WIDTH / 2 - 2, GAME_HEIGHT);
+    printf("CPU: %d", scoreCPU);
+}
+
+int showOptionsMenu() {
+    screenClear();
+    screenGotoxy(GAME_WIDTH / 2 - 8, GAME_HEIGHT / 2);
+    printf("Pressione 'p' para jogar sem barreiras");
+    screenGotoxy(GAME_WIDTH / 2 - 8, GAME_HEIGHT / 2 + 1);
+    printf("Pressione 'b' para jogar com barreiras");
+    screenGotoxy(GAME_WIDTH / 2 - 8, GAME_HEIGHT / 2 + 2);
+    printf("Pressione 'q' para sair");
+    screenUpdate();
+    char key;
+    while (1) {
+        if (keyhit()) {
+            key = readch();
+            if (key == START_KEY) {
+                barrierMode = 0;
+                break;
+            } else if (key == 'b') {
+                barrierMode = 1;
+                break;
+            } else if (key == EXIT_KEY) {
+                screenDestroy();
+                keyboardDestroy();
+                exit(0);
+            }
+        }
+    }
